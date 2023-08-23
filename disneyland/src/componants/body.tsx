773dd15331ceec03a27b1f1be0d9eff8e7b9b945
noufjafari <engnoufjafari@gmail.com>
2023-08-23 03:57:33 +0300

    change

--- disneyland/src/componants/body.tsx
+++ disneyland/src/componants/body.tsx
@@ -2,8 +2,8 @@ import Card from '../componants/Cards';
 import imag1 from '../assets/hd15961_2027jun06jun06_world_30th-anniversary-key-visual-shine-logo_16-9_tcm1861-231624$w~960$p~1$f~jpg.jpg';
 import imag2 from '../assets/hd16242_2050dec31_world_avengers-campus-key-visual_16-9_tcm1861-236755$w~960$p~1$f~jpg.jpg';
 import imag3 from '../assets/n015567c_2027sep30_world_hotel-new-york-the-art-of-marvel-exterior_16-9_tcm1861-256421$w~960$p~1$f~jpg.jpg';
-import img4 from '../assets/imgScROL1.jpg';
-import img5 from '../assets/imgScROL2.jpg';
+import img4 from '../assets/imgScROL1.png';
+import img5 from '../assets/imgScROL2.png';
 import img6 from '../assets/imgScROL3.png';
 import img7 from '../assets/imgScROL4.png';
 
@@ -22,25 +22,25 @@ function body() {
        <div className=" py-3 gap-6 flex flex-col justify-center w-96 text-center"><Card src={imag3} title='Make the magic last around the clock!​' contant='Discover the benefits of being at Disneyland Paris! With a Disney Hotel+Ticket package, immerse yourself in the magic just a short walk or shuttle ride away, greet Disney Characters.​​' button='Book a hotel + tiket '/></div>
             </div>
 
-            <div className="  flex flex-col justify-center text-center mt-20 mb-20 ">
+            <div className="  flex flex-col justify-center text-center mt-10 mb-10 ">
             <p className=" text-3xl font-medium ">The Disneyland Paris experience</p>
             <p className=" py-6 font-thin  ">Escape to the heart of the Disney, Pixar, MARVEL and Star <br></br>Wars universes that you love so much. Disneyland Paris is<br></br> full of activities for all ages and all tastes!</p>
             </div>
 
-          <div className=" flex flex-row justify-center gap-5">
-          <div className=" ">
+          <div className=" flex flex-row justify-center mt-5 mb-10 gap-x-8 gap-y-4">
+          <div className=" w-96 rounded-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300">
             <img src={img4} />
           </div>
-          <div className="">
+          <div className="w-96 rounded-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300">
             <img src={img5} />
           </div>
           </div>
 
-          <div className=" flex flex-row justify-center gap-5">
-          <div className="">
+          <div className=" flex flex-row justify-center gap-x-8 gap-y-4">
+          <div className="w-96 rounded-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300">
             <img src={img6} />
           </div>
-          <div className="">
+          <div className="w-96 rounded-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover: duration-300">
             <img src={img7} />
           </div>
           </div>
